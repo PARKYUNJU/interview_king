e3d2b42d6661b1f5dfa28e5804fcb3720e52b7e4
공상욱 <100742282+KongSangUk@users.noreply.github.com>
2022-07-23 03:16:52 +0900

    2022년 07월 23일 - 댓글 기능(am03:10)
    댓글 기능 뼈대
    controller - 23일 구현 예정
    service - 23일 구현 예정
    
    기본기능
    1. POST
    2. GET
    3. PUT
    4. DELLETE
    5. 시간표시 / 시간별 정렬
    
    추가 예정
    1.  조회수별 (기술문서 확인)
    2. 계층적 댓글 작성
    3. 피드백 받은 내용 수정
    4. 로그인/게시판 패키지 맞추기

--- interview_king(공상욱)/src/main/java/com/sparta/interview_king/repository/CommentRepository.java
+++ interview_king(공상욱)/src/main/java/com/sparta/interview_king/repository/CommentRepository.java
@@ -1,4 +1,10 @@
 package com.sparta.interview_king.repository;
 
-public class CommentRepository {
+import com.sparta.interview_king.model.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findAllByPostidOrderByCreatedAtDesc(Long postId);
 }
--- interview_king(공상욱)/src/main/java/com/sparta/interview_king/service/CommentService.java
+++
@@ -1,4 +0,0 @@
-package com.sparta.interview_king.service;
-
-public class CommentService {
-}
